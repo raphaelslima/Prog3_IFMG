2b1f392291dfde3a4171a91c182f7c8d51c5529d
Nathan0013 <nathan.siqueira1415@gmail.com>
2024-08-04 15:02:33 -0300

    Volume de Tweets - Questao 1

---
+++ ListaEncadeada.java
@@ -0,0 +1,184 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ListaEncadeada<T> {
+    private No<T> inicio;
+    private No<T> ultimo;
+    private int tamanho = 0;
+
+    public void adicionar(String tweetID, String handLabel, String AnnotatorID){
+        No<T> celula = new No(tweetID, handLabel, AnnotatorID);
+        if(this.tamanho == 0){
+            this.inicio = celula;
+        }else{
+            this.ultimo.setProximo(celula);;
+        }
+        this.ultimo = celula;
+        this.tamanho++;   
+    }
+
+    public void adicionar(int pos, String tweetID, String handLabel, String AnnotatorID){
+        if(pos == 0){
+            if(pos == this.tamanho){
+                this.adicionar(tweetID, handLabel, AnnotatorID);
+            } else {
+                No<T> novoNo = new No(tweetID, handLabel, AnnotatorID, this.inicio);
+                this.inicio = novoNo;
+                this.tamanho++;
+            }
+        } else if(pos == this.tamanho){
+            this.adicionar(tweetID, handLabel, AnnotatorID);
+        } else if(pos > this.tamanho || pos < 0){
+            throw new IllegalArgumentException("Posição inválida");
+        } else{
+           No<T> noAnterior = this.buscaNo(pos-1);
+           No<T> noProximo = noAnterior.getProximo();
+
+           No<T> novoNo = new No(tweetID, handLabel, AnnotatorID, noProximo);
+           noAnterior.setProximo(novoNo);
+           this.tamanho++;
+        }
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void percorreLista(){
+        if(this.tamanho == 0){
+            System.out.println("[]");
+        } else{
+            No<T> atual = this.inicio;
+
+            if(atual.getProximo() == null){
+                System.out.println(atual.getTweetID());
+            }else{
+                for(int i = 0; i<this.tamanho; i++){
+                    System.out.println(atual.getTweetID());
+                    atual = atual.getProximo();
+                }
+            } 
+        }             
+    }
+
+    public void limpa(){
+        No<T> atual = this.inicio;
+        for(int i = 0; i < this.tamanho; i++){
+            No <T> proximo = atual.getProximo();
+            atual.setElemento(null, null, null);
+            atual.setProximo(null);
+            atual = proximo;
+        }
+
+        this.inicio = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+    }
+
+    private No<T> buscaNo(int pos){
+        if (!(pos < this.tamanho && pos >= 0)) {
+            throw new IllegalArgumentException("Posição não existe");
+        }
+
+        No<T> atual = this.inicio;
+        for(int i = 0; i < pos; i++){
+            atual = atual.getProximo();
+        }
+        return atual;
+    }
+
+    public T buscaPorPosicao(int posicao){
+        return this.buscaNo(posicao).getTweetID();
+    }
+
+    public int busca(T tweetID){
+        No<T> atual = this.inicio;
+        int pos = 0;
+
+        for(int i = 0; i < this.tamanho; i++){
+
+            if(atual.getTweetID().equals(tweetID)){
+                return pos;
+            }
+
+            pos++;
+            atual = atual.getProximo();
+        }
+        
+        return -1;
+    }
+
+    public T removePrimeiro(){
+        if(this.tamanho == 0){
+            throw new RuntimeException("Lista vazia");
+        } else {
+            T removido = this.inicio.getTweetID();
+            this.inicio = this.inicio.getProximo();
+            this.tamanho--;
+
+            if (this.tamanho == 0) {
+                this.ultimo = null;
+            }
+            return removido;
+        }
+    }
+
+    public T removeUltimo(){
+        if(this.tamanho == 0){
+            throw new RuntimeException("Lista vazia");
+        }
+        if(this.tamanho == 1){
+            this.removePrimeiro();
+        }
+
+        No<T> noAnterior = this.buscaNo(this.tamanho -2);
+        T removido = noAnterior.getProximo().getTweetID();
+        noAnterior.setProximo(null);
+        this.ultimo = noAnterior;
+        this.tamanho--;
+        return removido;
+    }
+
+    public T removeMeio(int pos){
+        if(!(pos >= 0 || pos < this.tamanho)){
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        if(pos == 0){
+            return this.removePrimeiro();
+        } else if (pos == this.tamanho-1) {
+            return this.removeUltimo();
+        } else {
+            No<T> noAnterior = this.buscaNo(pos-1);
+            No<T> noAtual = noAnterior.getProximo();
+            noAnterior.setProximo(noAtual.getProximo());
+            noAtual.setProximo(null);
+            this.tamanho--;
+            return noAtual.getTweetID();
+        }
+    }
+
+    public void tranformaArquivoParaLista(String file, ListaEncadeada<T> ListaEncadeada){
+        boolean pular1Linha = true;
+        try (Scanner scanner = new Scanner(new File(file))) {
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                if (!pular1Linha) {
+                    int primeiraVirgula = linha.indexOf(',');
+                    int segundaVirgula = linha.indexOf(',', primeiraVirgula + 1);
+        
+                    String tweetID = linha.substring(0, primeiraVirgula);
+                    String handLabel = linha.substring(primeiraVirgula + 1, segundaVirgula);
+                    String AnnotatorID = linha.substring(segundaVirgula + 1);
+
+                    ListaEncadeada.adicionar(tweetID, handLabel, AnnotatorID);
+                }else{
+                    pular1Linha = false;
+                }
+                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ Principal.java
@@ -0,0 +1,134 @@
+import java.io.*;
+import java.util.*;
+
+class Principal {
+
+    public static void main(String[] args) {
+
+        // TP1
+        /*String arquivo = "./Dataset_Facebook.csv";
+
+        int hora = 0;
+        int semana = 0;
+        int mes = 0;
+
+        int likesTotais = 0, qtdeInteracao = 0;
+        String tipo = "", categoria = "", comentarios = "", compartilhamentos = "";
+        boolean ehPago = true;
+        boolean pular1 = false;
+
+        int totalAlcance = 0, totalImpressoes = 0, engajadosUsuarios = 0, clientes = 0, consumo = 0,
+                impressoesPorLike = 0, alcancePorLike = 0, engajadosLike = 0;
+
+        PostLifetime postLifetime = new PostLifetime(totalAlcance, totalImpressoes, engajadosUsuarios,
+                clientes, consumo, impressoesPorLike, alcancePorLike, engajadosLike);
+
+        PostInfos postInfos = new PostInfos(likesTotais, tipo, qtdeInteracao, categoria, comentarios, compartilhamentos,
+                ehPago);
+
+        PostTime postTime = new PostTime(hora, semana, mes);
+
+        try (Scanner scanner = new Scanner(new File(arquivo))) {
+            scanner.useDelimiter(",");
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                if (pular1 == true) {
+                    String[] campos = linha.split(";", 19);
+                    postInfos = new PostInfos(Integer.parseInt(campos[0]), campos[1], Integer.parseInt(campos[2]),
+                            campos[3], campos[4], campos[5], Boolean.parseBoolean(campos[6]));
+
+                    postLifetime = new PostLifetime(Integer.parseInt(campos[7]), Integer.parseInt(campos[8]),
+                            Integer.parseInt(campos[9]), Integer.parseInt(campos[10]), Integer.parseInt(campos[11]),
+                            Integer.parseInt(campos[12]), Integer.parseInt(campos[13]), Integer.parseInt(campos[14]));
+
+                    postTime = new PostTime(Integer.parseInt(campos[12]), Integer.parseInt(campos[13]),
+                            Integer.parseInt(campos[14]));
+
+                    postInfos.imprimeTudo();
+                    postLifetime.imprimeTudo();
+                    postTime.imprimeTudo();
+
+                    System.out.println(
+                            "--------------------------------------------------------------------------------------------------");
+                    System.out.println("");
+                }
+                pular1 = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        //TP2
+        ListaEncadeada listaTeste = new ListaEncadeada<>();
+        ListaEncadeada listaAlba = new ListaEncadeada<>();
+        ListaEncadeada listaBosn = new ListaEncadeada<>();
+        ListaEncadeada listaCroa = new ListaEncadeada<>();
+        ListaEncadeada listaEngl = new ListaEncadeada<>();
+        ListaEncadeada listaGerm = new ListaEncadeada<>();
+        ListaEncadeada listaHung = new ListaEncadeada<>();
+        ListaEncadeada listaPoli = new ListaEncadeada<>();
+        ListaEncadeada listaPort = new ListaEncadeada<>();
+        ListaEncadeada listaRuss = new ListaEncadeada<>();
+        ListaEncadeada listaSerb = new ListaEncadeada<>();
+        ListaEncadeada listaSlovak = new ListaEncadeada<>();
+        ListaEncadeada listaSlovenian = new ListaEncadeada<>();
+        ListaEncadeada listaSpan = new ListaEncadeada<>();
+        ListaEncadeada listaSwed = new ListaEncadeada<>();
+        
+        //Chama os arquivos
+        String arquivoTeste = "./datasetTwitters/teste.csv";
+        String arquivoAlba = "./datasetTwitters/Albanian_Twitter_Sentiment.csv";
+        String arquivoBosn = "./datasetTwitters/Bosnian_Twitter_Sentiment.csv";
+        String arquivoCroa = "./datasetTwitters/Croatian_Twitter_Sentiment.csv";
+        String arquivoEngl = "./datasetTwitters/English_Twitter_Sentiment.csv";
+        String arquivoGerm = "./datasetTwitters/German_Twitter_Sentiment.csv";
+        String arquivoHung = "./datasetTwitters/Hungarian_Twitter_Sentiment.csv";
+        String arquivoPoli = "./datasetTwitters/Polish_Twitter_Sentiment.csv";
+        String arquivoPort = "./datasetTwitters/Portuguese_Twitter_Sentiment.csv";
+        String arquivoRuss = "./datasetTwitters/Russian_Twitter_Sentiment.csv";
+        String arquivoSerb = "./datasetTwitters/Serbian_Twitter_Sentiment.csv";
+        String arquivoSlovak = "./datasetTwitters/Slovak_Twitter_Sentiment.csv";
+        String arquivoSlovenian = "./datasetTwitters/Slovenian_Twitter_Sentiment.csv";
+        String arquivoSpan = "./datasetTwitters/Spanish_Twitter_Sentiment.csv";
+        String arquivoSwed = "./datasetTwitters/Swedish_Twitter_Sentiment.csv";
+
+        
+        //Chama o método tranformaArquivoParaLista
+        listaTeste.tranformaArquivoParaLista(arquivoTeste, listaTeste);
+        listaAlba.tranformaArquivoParaLista(arquivoAlba, listaAlba);
+        listaBosn.tranformaArquivoParaLista(arquivoBosn, listaBosn);
+        listaCroa.tranformaArquivoParaLista(arquivoCroa, listaCroa);
+        listaEngl.tranformaArquivoParaLista(arquivoEngl, listaEngl);
+        listaGerm.tranformaArquivoParaLista(arquivoGerm, listaGerm);
+        listaHung.tranformaArquivoParaLista(arquivoHung, listaHung);
+        listaPoli.tranformaArquivoParaLista(arquivoPoli, listaPoli);
+        listaPort.tranformaArquivoParaLista(arquivoPort, listaPort);
+        listaRuss.tranformaArquivoParaLista(arquivoRuss, listaRuss);
+        listaSerb.tranformaArquivoParaLista(arquivoSerb, listaSerb);
+        listaSlovak.tranformaArquivoParaLista(arquivoSlovak, listaSlovak);
+        listaSlovenian.tranformaArquivoParaLista(arquivoSlovenian, listaSlovenian);
+        listaSpan.tranformaArquivoParaLista(arquivoSpan, listaSpan);
+        listaSwed.tranformaArquivoParaLista(arquivoSwed, listaSwed);
+
+        //Printa o volume de Tweets de cada idioma
+        System.out.println("Volume de Tweets Albania: " + listaAlba.getTamanho());
+        System.out.println("Volume de Tweets Bósnia: " + listaBosn.getTamanho());
+        System.out.println("Volume de Tweets Croácia: " + listaCroa.getTamanho());
+        System.out.println("Volume de Tweets Inglês: " + listaEngl.getTamanho());
+        System.out.println("Volume de Tweets Alemão: " + listaGerm.getTamanho());
+        System.out.println("Volume de Tweets Húngaro: " + listaHung.getTamanho());
+        System.out.println("Volume de Tweets Polonês: " + listaPoli.getTamanho());
+        System.out.println("Volume de Tweets Português: " + listaPort.getTamanho());
+        System.out.println("Volume de Tweets Russo: " + listaRuss.getTamanho());
+        System.out.println("Volume de Tweets Sérvio: " + listaSerb.getTamanho());
+        System.out.println("Volume de Tweets Eslovaco: " + listaSlovak.getTamanho());
+        System.out.println("Volume de Tweets Esloveno: " + listaSlovenian.getTamanho());
+        System.out.println("Volume de Tweets Espanhol: " + listaSpan.getTamanho());
+        System.out.println("Volume de Tweets Sueco: " + listaSwed.getTamanho());
+        System.out.println("Volume de Tweets Teste: " + listaTeste.getTamanho());
+
+        listaTeste.percorreLista();
+    }
+
+}
